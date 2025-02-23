5569a62de6d6cc0462ebc2ec37e075d4055d2b4e
TheDevOpser <sylvain.anglade@gmail.com>
2025-02-23 10:07:00 +0100

    add OAuth2 authentication for Google, Discord, and Battle.net; implement user menu and navbar components

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,4 @@
+import './bootstrap.js';
 import './styles/app.css';
+import '@symfony/ux-live-component';
+
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -28,5 +30,31 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/connect/google', name: 'connect_google')]
+    public function connectGoogle(ClientRegistry $clientRegistry): Response
+    {
+        return $clientRegistry->getClient('google')->redirect();
+    }
+
+    #[Route('/connect/discord', name: 'connect_discord')]
+    public function connectDiscord(ClientRegistry $clientRegistry): Response
+    {
+        return $clientRegistry->getClient('discord')->redirect();
+    }
+
+    #[Route('/connect/battlenet', name: 'connect_battlenet')]
+    public function connectBattlenet(ClientRegistry $clientRegistry): Response
+    {
+        return $clientRegistry->getClient('battlenet')->redirect();
+    }
+
+    #[Route('/connect/google/check', name: 'connect_google_check')]
+    #[Route('/connect/discord/check', name: 'connect_discord_check')]
+    #[Route('/connect/battlenet/check', name: 'connect_battlenet_check')]
+    public function check(Request $request): void
+    {
+
     }
 }
