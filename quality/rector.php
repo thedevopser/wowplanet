2e2eac73a94c9905c8237b13b6fc137571bb2981
TheDevOpser <sylvain.anglade@gmail.com>
2025-12-06 19:23:17 +0100

    chore(quality): create first entoity to avoid errors on CI

--- quality/rector.php
+++ quality/rector.php
@@ -6,6 +6,7 @@ use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -17,6 +18,8 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/../vendor',
         __DIR__ . '/../var',
         __DIR__ . '../tests/Fixtures',
+        // Désactivation de la promotion des propriétés dans le constructeur
+        ClassPropertyAssignToConstructorPromotionRector::class,
     ]);
 
     $rectorConfig->sets([
